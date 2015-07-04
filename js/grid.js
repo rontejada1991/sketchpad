278f11f449cba734cbb456bad216dea2dd17e4d1
Ronald Tejada <rontejada91@gmail.com>
2015-07-03 20:24:29 -0400

    Clear button added. Makes new grids. Cells stretch evenly within container.

--- js/grid.js
+++ js/grid.js
@@ -1,8 +1,9 @@
 // Grid size
-var rows = 16;
-var columns = 16;
+var current_columns = 16;
+var current_rows = 16;
 
-$(document).ready(function() {
+// Makes our grid based on the number of columns, rows given
+var makeGrid = function(columns, rows) {
   // Empty row
   $row = $("<div class='row'></div>");
 
@@ -11,8 +12,31 @@ $(document).ready(function() {
     $row.append("<div class='square'></div>");
   }
 
+  // Fills the rest of the columns with rows
   for (var i = 0; i < columns; i++) {
     $("div#container").append($row.clone());
   }
+
+};
+
+// Stretches the size of our cells based on the number of rows/columns selected and the size of our container
+var stretchCells = function(columns, rows) {
+  // Grab the height and width of our container
+  var container_height = $("div#container").height();
+  var container_width = $("div#container").width();
+
+  // Calculate each individual each individual cells height and width to fit our container evenly
+  var cell_height = container_height / rows;  
+  var cell_width = container_width / columns;
+
+  // Change our selectors height and width properties for our cells
+  $(".square").css("height", cell_height + "px");
+  $(".square").css("width", cell_width + "px");
+
+};
+
+$(document).ready(function() {
+  makeGrid(current_columns, current_rows);
+  stretchCells(current_columns, current_rows);
 
 });
--- js/mouse_controls.js
+++ js/mouse_controls.js
@@ -1,7 +1,42 @@
-$(document).ready(function() {
-
+// Assign hover properties to our cells
+function assignCellHover() {
   $("div.square").hover(function() {
     $(this).addClass("hovered");
   });
+}
+
+// Clearing out our grid on click of our clear button
+function cleargrid() {
+  $("div.square").removeClass("hovered");
+
+  // Asks / Stores how many cells per side
+  var gridSize = prompt("How many squares per side should I make your canvas? (Up to 128)", 64);
+
+  // Check for validation if less than min / max or not a number
+  while (!$.isNumeric(gridSize) || gridSize < 1 || gridSize > 128)
+  if (gridSize < 1) {
+    var gridSize = prompt("Please enter a higher number.");
+  } else if (gridSize > 128) {
+    var gridSize = prompt("Please enter a lower number.", 64);      
+  } else {
+    var gridSize = prompt("Please enter a valid number.", 64);
+  }
+
+  // Assign new grid size to our rows and column variables
+  current_columns = gridSize;
+  current_rows = gridSize;
+
+  // Remove current grid
+  $("div#container").empty();
+
+  // Create a new grid and assign the hover property to each
+  makeGrid(current_columns, current_rows);
+  stretchCells(current_columns, current_rows);
+  assignCellHover();
+
+}
+
+$(document).ready(function() {
+  assignCellHover();
 
 });
